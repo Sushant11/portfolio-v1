884aa531a7b3b4c728e9a0f20ed58984da3525b3
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-07-08 22:38:01 +0545

    ..

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial,Super Nice!</h1>
       <p>
         What do I like to do? Lots of course but definitely enjoy building
         websites.
