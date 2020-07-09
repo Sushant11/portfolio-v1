6988071676f5c7516b759c6a856377ba8d98f610
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-07-09 21:36:00 +0545

    initialSetups

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,30 @@
+const config = require('./src/config')
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: config.siteTitle,
+    siteUrl: config.siteUrl,
+    description: config.siteDescription,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: config.colors.softWhite,
+        theme_color: config.colors.softDark,
+        display: `minimal-ui`,
+        icon: `src/images/favicons/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,26 +32,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: `gatsby-plugin-styled-components`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        // Add any options here
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicons/favicon.ico`, // This path is relative to the root of the site.
-      },
-    },
+   
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/config.js
@@ -0,0 +1,55 @@
+module.exports = {
+    siteTitle: 'Sushant Chitrakar | Frontend Developer',
+  siteDescription:
+    'Sushant Chitrakar is a Kathmandu based Frontend Developer with a keen eye on both design as well as development of interactive and visual web apps.',
+  siteKeywords:
+    'Sushant Chitrakar, Sushant, Chitrakar, Sushant11, frontend, frontend-developer, frontend-engineer, software engineer, front-end engineer, web developer, javascript, northeastern',
+//   siteUrl: 'https://sushantchitrakar.com.np',
+  siteLanguage: 'en_US',
+  name: 'Sushant Chitrakar',
+  location: 'Kathmandu, NPL',
+  email: 'sushantchitrakar@gmail.com',
+  github: 'https://github.com/Sushant11',
+  instaHandle: '@sus.chtre',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/Sushant11',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/sushant-chitrakar',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/sus.chtre',
+    },
+    {
+      name: 'Dribble',
+      url: 'https://dribbble.com/Chtre',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    softWhite: '#F9F9F9', 
+    softDark: '#252525      ',
+    darkGrey: '#363636',
+  },
+}
